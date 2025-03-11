e1e6416e0fb29a523ea733464c364b77d7b9d586
git-Sahitya <sahitya5289@gmail.com>
2025-03-11 19:22:51 +0530

    Implement user logout functionality and add token blacklist model

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model.js");
 const userService = require("../services/user.service.js");
-const { validationResult } = require("express-validator");
+const { validationResult, cookie } = require("express-validator");
+const blackListTokenModel = require('../models/blacklistToken.model.js')
 
 // controller for register user
 module.exports.registerUser = async (req, res, next) => {
@@ -56,7 +57,17 @@ module.exports.loginUser = async (req, res, next) => {
   res.status(200).json({ token, user });
 };
 
+// profile
 module.exports.getUserProfile = async (req,res,next) => {
   res.status(200).json(req.user)
+}
+
+// Logout 
+ module.exports.logoutUser = async (req,res,next) => {
+  res.clearCookie('token')
+  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+ 
+  await blackListTokenModel.create( { token})
+  res.status(200).json( { message : "Logged out"})
 }
   
