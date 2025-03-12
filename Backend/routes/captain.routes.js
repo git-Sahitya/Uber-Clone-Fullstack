63ad216ac231d42e4d70dc4ca7aed670c041b344
git-Sahitya <sahitya5289@gmail.com>
2025-03-12 10:29:49 +0530

    Add captain registration functionality with validation and routes

---
+++ Backend/routes/captain.routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const {body} = require('express-validator')
+const captainController = require('../controllers/captain.controller')
+
+router.post('/register', [
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('fullname.firstname').isLength( { min : 3}).withMessage('Fullname is required'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    body('vehicle.color').isLength( { min : 3}).withMessage('Vehicle color is required'),
+    body('vehicle.plate').isLength( { min : 3}).withMessage('Vehicle plate is required'),
+    body('vehicle.capacity').isInt({ min: 1 }).withMessage('Vehicle capacity must be at least 1'),
+    body('vehicle.vehicleType').isIn(['car' , 'motercycle' , 'auto']).withMessage('Vehicle type is required')
+],
+ captainController.registerCaptain
+)
+
+module.exports = router
