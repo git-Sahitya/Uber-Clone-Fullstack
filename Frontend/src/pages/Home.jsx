b3bba819b5fe664297f465daa978f5155ee597c9
git-Sahitya <sahitya5289@gmail.com>
2025-03-22 19:24:02 +0530

    Add GSAP animations to Home component and update CaptainHome layout

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
   const submitHandler = (e) => {
     e.preventDefault();
   };
+
+  useGSAP(function () {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: " 70%",
+      });
+    } else {
+      gsap.to(panelRef.current, {
+        height: " 0%",
+      });
+    }
+  }, [panelOpen]);
+
   return (
     <div className="h-screen relative">
       <img
@@ -22,7 +39,7 @@ const Home = () => {
         />
       </div>
       <div className=" flex flex-col justify-end h-screen absolute top-0 w-full ">
-        <div className="h-[30%] relative bg-white p-5">
+        <div className="h-[30%] relative bg-white p-6">
           <h4 className="text-2xl font-semibold">Find a trip</h4>
           <form
             onSubmit={(e) => {
@@ -33,24 +50,30 @@ const Home = () => {
             <input
               className="bg-[#eee] px-12 py-2 text-lg rounded-lg  w-full mt-5 "
               type="text"
+              onClick={() => {
+                setPanelOpen(true);
+              }}
               value={pickup}
-              onChange={(e)=>{
-                setPickup(e.target.value)
+              onChange={(e) => {
+                setPickup(e.target.value);
               }}
               placeholder="Add a pick-up loaction"
             />
             <input
               className="bg-[#eee] px-12 py-2 text-lg rounded-lg  w-full mt-3 "
               type="text"
+              onClick={() => {
+                setPanelOpen(true);
+              }}
               value={destination}
-              onChange={(e)=>{
-                setDestination(e.target.value)
+              onChange={(e) => {
+                setDestination(e.target.value);
               }}
               placeholder="Add a destination"
             />
           </form>
         </div>
-        <div className="h-[0] bg-red-500  "></div>
+        <div ref={panelRef} className="h-[0] bg-red-500  "></div>
       </div>
     </div>
   );
