03a533e1600c3ab1a9a2cb28260a50cec038f1ff
git-Sahitya <sahitya5289@gmail.com>
2025-03-25 19:00:16 +0530

    Add ConfirmRide and VehiclePanel components; integrate them into Home and LocationSearchPanel for improved ride confirmation flow

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -3,8 +3,6 @@ import "remixicon/fonts/remixicon.css";
 
 const LocationSearchPanel = (props) => {
   // sample array for locations
-  console.log(props);
-  
 
   const loactions = [
     "B 46/142, Mahamanapuri Colony , BHU ,Varanasi",
@@ -15,24 +13,25 @@ const LocationSearchPanel = (props) => {
 
   return (
     <div>
-       {/* This is just a sample data */}
+      {/* This is just a sample data */}
 
-       {loactions.map(function(elem , idx){
-        return <div key={idx} onClick={()=>{
-          props.setVehiclePanel(true)
-          props.setPanelOpen(false)
-        }} className="flex items-center border-1 p-2  border-gray-200 active:border-black rounded-lg gap-2 justify-start">
-        <h2 className=" bg-[#eee] h-7 w-8 flex items-center justify-center  rounded-full">
-          <i className="ri-map-pin-2-line "></i>
-        </h2>
-        <h4 className="font-normal">
-         {elem}
-        </h4>
-      </div>
-       })}
-      
-      
-      
+      {loactions.map(function (elem, idx) {
+        return (
+          <div
+            key={idx}
+            onClick={() => {
+              props.setVehiclePanel(true);
+              props.setPanelOpen(false);
+            }}
+            className="flex items-center border-1 p-2  border-gray-200 active:border-black rounded-lg gap-2 justify-start"
+          >
+            <h2 className=" bg-[#eee] h-7 w-8 flex items-center justify-center  rounded-full">
+              <i className="ri-map-pin-2-line "></i>
+            </h2>
+            <h4 className="font-normal">{elem}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,15 +3,21 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import "remixicon/fonts/remixicon.css";
 import LocationSearchPanel from "../components/LocationSearchPanel";
+import VehiclePanel from "../components/VehiclePanel";
+import ConfirmRide from "../components/ConfirmRide";
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
   const [vehiclePanel, setVehiclePanel] = useState(false);
+  const [confirmRidePanel, setConfirmRidePanel] = useState(false)
   const vehiclePanelRef = useRef(null);
+  const confirmRidePanelRef = useRef(null);
   const panelRef = useRef(null);
   const panelCloseRef = useRef(null);
+
+  
   const submitHandler = (e) => {
     e.preventDefault();
   };
@@ -56,6 +62,21 @@ const Home = () => {
       }
     },
     [vehiclePanel]
+  );
+  // Confirm Ride 
+  useGSAP(
+    function () {
+      if (confirmRidePanel) {
+        gsap.to(confirmRidePanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(confirmRidePanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [confirmRidePanel]
   );
 
   return (
@@ -127,76 +148,18 @@ const Home = () => {
 
       <div
         ref={vehiclePanelRef}
-        className="fixed w-full z-10 bottom-0 translate-y-full  bg-white p-3 py-10 px-3 pt-15"
+        className="fixed w-full z-10 bottom-0 translate-y-full  bg-white py-10 px-3 pt-15"
       >
-        <h5 className=" p-2 text-center w-[90%]  absolute top-0 right-2 "
-         onClick={()=>{
-          setVehiclePanel(false)
-         }}
-        ><i className= " text-2xl  text-gray-300 ri-arrow-down-wide-line"></i></h5>
-        <h3 className="text-2xl font-semibold mb-5">Choose a vehicle</h3>
-
-        <div className="flex w-full  border-1 border-white active:border-black   mb-2 rounded-xl p-3  items-center justify-between">
-          <img
-            className="h-12"
-            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1712027307/assets/42/eb85c3-e2dc-4e95-a70d-22ee4f08015f/original/Screenshot-2024-04-01-at-9.08.07p.m..png"
-            alt=""
-          />
-          <div className=" w-1/2">
-            <h4 className="font-medium text-base">
-              UberGo{" "}
-              <span>
-                <i className="ri-user-3-fill">5</i>
-              </span>
-            </h4>
-            <h5 className="font-medium text-sm"> 2 mins away</h5>
-            <p className="font-normal text-xs text-gray-600">
-              Affordable, compact rides
-            </p>
-          </div>
-          <h2 className="text-lg font-medium">$4.99</h2>
-        </div>
-        <div className="flex w-full  border-1 border-white active:border-black mb-2 rounded-xl p-3  items-center justify-between">
-          <img
-            className="h-12"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfZk9YnqF1LyugOEqiW3Wot1QKVZhVw202uQ&s"
-            alt=""
-          />
-          <div className=" w-1/2">
-            <h4 className="font-medium text-base">
-              Moto{" "}
-              <span>
-                <i className="ri-user-3-fill">1</i>
-              </span>
-            </h4>
-            <h5 className="font-medium text-sm"> 3 mins away</h5>
-            <p className="font-normal text-xs text-gray-600">
-              Affordable, motorcycle rides
-            </p>
-          </div>
-          <h2 className="text-lg font-medium">$1.45</h2>
-        </div>
-        <div className="flex w-full  border-1 border-white active:border-black mb-2 rounded-xl p-3  items-center justify-between">
-          <img
-            className="h-12 -ml-3"
-            src="https://images.cnbctv18.com/uploads/2023/10/uber-auto.jpg"
-            alt=""
-          />
-          <div className=" -ml-3 w-1/2">
-            <h4 className="font-medium text-base">
-              UberAuto{" "}
-              <span>
-                <i className="ri-user-3-fill">3</i>
-              </span>
-            </h4>
-            <h5 className="font-medium text-sm"> 2 mins away</h5>
-            <p className="font-normal text-xs text-gray-600">
-              Affordable, Auto rides
-            </p>
-          </div>
-          <h2 className="text-lg font-medium">$2.92</h2>
-        </div>
+        <VehiclePanel setConfirmRidePanel={setConfirmRidePanel} setVehiclePanel={setVehiclePanel} />
       </div>
+
+      <div
+        ref={confirmRidePanelRef}
+        className="fixed w-full z-10 bottom-0 translate-y-full  bg-white  py-6 px-3 pt-15"
+      >
+        <ConfirmRide/>
+      </div>
+      
     </div>
   );
 };
