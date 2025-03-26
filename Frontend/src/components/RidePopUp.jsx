066156e55193670ccdaa3371d298fcf4377b1950
git-Sahitya <sahitya5289@gmail.com>
2025-03-26 11:40:31 +0530

    Add CaptainDetails and RidePopUp components to enhance CaptainHome with user information and ride notifications

---
+++ Frontend/src/components/RidePopUp.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const RidePopUp = () => {
+  return (
+    <div>
+      <h5
+        className=" p-2 text-center w-[90%]  absolute top-0 right-2 "
+        onClick={() => {
+        }}
+      >
+        <i className=" text-2xl  text-gray-300 ri-arrow-down-wide-line"></i>
+      </h5>
+      <h3 className="text-2xl font-semibold mb-5">New Ride Available! </h3>
+      <div className="flex items-center justify-between p-2 bg-yellow-400 rounded-lg mt-4">
+        <div className="flex items-center gap-3 ">
+            <img className="h-12 w-11 rounded-full object-cover" src="https://img.freepik.com/free-vector/organic-flat-dia-motorista-illustration_23-2148961409.jpg?uid=R193013355&ga=GA1.1.1896811457.1729647436&semt=ais_hybrid" alt="" />
+        <h2 className="text-lg font-medium">Harsh Singh</h2>
+        </div>
+        <h5 className="text-lg font-medium">2.2Km</h5>
+      </div>
+      <div className=" flex flex-col gap-2 justify-between items-center">
+        <div className="w-full mt-5">
+          <div className="flex items-center gap-5 p-3 border-b-1">
+            <i className=" text-lg ri-map-pin-range-fill"></i>
+            <div>
+              <h3 className="text-base font-medium">B/54</h3>
+              <p className="text-sm text-gray-600"> Lanka, Varanasi</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3 border-b-1">
+            <i className=" text-lg ri-map-pin-range-fill"></i>
+            <div>
+              <h3 className="text-base font-medium">Lane no.9</h3>
+              <p className="text-sm text-gray-600"> Cantt, Varanasi</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-5 p-3 ">
+            <i className=" ri-currency-fill"></i>
+            <div>
+              <h3 className="  text-base font-medium">$2.99</h3>
+              <p className="text-sm text-gray-600"> Cash Cash</p>
+            </div>
+          </div>
+        </div>
+        <button
+          onClick={() => {}}
+          className="w-full mt-4 bg-green-500 text-white font-semibold p-2 rounded-lg"
+        >
+          Confirm
+        </button>
+
+        <button
+          onClick={() => {}}
+          className="w-full mt-1 bg-gray-300 text-gray-700 font-semibold p-2 rounded-lg"
+        >
+          Ignore
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RidePopUp;
